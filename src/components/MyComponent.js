df9294fc2d73ca4613f32b19699c8db14d4a74a0
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-03-09 23:04:03 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+// class component
+// function component
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    //JXS
+    render() {
+        return (
+            <div>my first component
+
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
